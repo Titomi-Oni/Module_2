17e6996596c2bab5a22d672be790db6120ce3fe8
Titomi Oni <onititomi@gmail.com>
2026-04-16 19:53:30 -0600

    Sum of whole number

---
+++ Lesson_2/main.py
@@ -0,0 +1,30 @@
+n = "Hello"
+for i in n:
+    print(i)
+
+# range( stop)
+# range(start, stop)
+# range(start, stop, step)
+
+print ("Print from 0 to 4")
+
+# range( stop)
+
+for i in range (5):
+    print (i)
+
+print ("Print from 2 to 5")
+
+# range(start, stop)
+
+for i in range (2,6):
+    print (i)
+
+#step 2 = skip 1 number
+
+print ("Print from 1 to 10 but step 2")
+
+# range(start, stop, step)
+
+for i in range (1,11,2):
+    print (i)
